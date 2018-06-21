f234b62dc8cb21e6b7771de400a6c818cc05d684
Wenbo Lan <leo92613@gmail.com>
2018-06-21 15:54:52 -0400

    Init

---
+++ Assets/CaptainsMess/Extension.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+
+public static class Extension{
+    public static int Hash(this string str)
+    {
+        int hash = 0;
+        foreach(char c in str)
+        {
+            hash += (int)c;
+        }
+        return hash;
+    }
+}
